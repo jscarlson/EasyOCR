52dc07a8348fb7943974aae30a2f6cf3655c973b
jscarlson <jcarlson238@gmail.com>
2022-10-02 21:19:16 -0400

    dev

--- easyocr/easyocr.py
+++ easyocr/easyocr.py
@@ -104,7 +104,6 @@ class Reader(object):
         separator_list = {}
 
         if recog_network in ['standard'] + [model for model in recognition_models['gen1']] + [model for model in recognition_models['gen2']]:
-            print("GOT HERE 1")
             if recog_network in [model for model in recognition_models['gen1']]:
                 model = recognition_models['gen1'][recog_network]
                 recog_network = 'generation1'
@@ -205,7 +204,9 @@ class Reader(object):
             with open(os.path.join(self.user_network_directory, recog_network+ '.yaml'), encoding='utf8') as file:
                 recog_config = yaml.load(file, Loader=yaml.FullLoader)
             with open(recog_config["character_list"]) as f:
-                recog_config["character_list"] = "".join(chr(int(i)) for i in f.read().split())
+                chars_from_file = "".join(chr(int(i)) for i in f.read().split())
+                print(chars_from_file[:100])
+                recog_config["character_list"] = chars_from_file
             imgH = recog_config['imgH']
             available_lang = recog_config['lang_list']
             self.setModelLanguage(recog_network, lang_list, available_lang, available_lang)
