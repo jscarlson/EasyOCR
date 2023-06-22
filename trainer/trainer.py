1b91188d77b25e8f76282f4a796c7ecc02b33c47
jscarlson <jcarlson238@gmail.com>
2023-06-22 07:31:42 -0400

    dev

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -17,7 +17,7 @@ def get_config(file_path):
     opt = AttrDict(opt)
     if os.path.isfile(opt.lang_char):
         with open(opt.lang_char) as f:
-            opt.character = "".join(chr(int(i)) for i in f.read().split())
+            opt.character = "".join(i for i in f.read().split())
             opt.character += " "
     elif opt.lang_char in ["ja", "en"]:
         reader = easyocr.Reader([opt.lang_char], gpu=True)
